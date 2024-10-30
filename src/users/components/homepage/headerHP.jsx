0d7743eee908312cfdd76b1beebb70c94119688e
LucHandsome <96497540+LucHandsome@users.noreply.github.com>
2024-10-30 21:09:29 +0700

    Cập nhật 3010

--- src/users/components/homepage/headerHP.jsx
+++ src/users/components/homepage/headerHP.jsx
@@ -30,7 +30,7 @@ function HeaderHP() {
             <div className="flex justify-between items-center">
                 <img src={LogoWeb} alt="Logo Web" className="w-24 sm:w-auto" /> 
                 <div className="flex gap-2 sm:gap-4 items-center">
-                    <span className="text-[#ff7e00] text-sm sm:text-base font-bold">Chào {userData.name} </span> 
+                    <span className="text-[#ff7e00] text-sm sm:text-base font-bold">Chào khách hàng </span> 
                     <img className="w-10 sm:w-[50px] h-10 sm:h-[50px] border rounded-full" src={VietNam} alt="Việt Nam" /> 
                 </div>
             </div>
